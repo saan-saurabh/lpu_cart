095e417d1d70f5f13bc127cca957f02c0f47998e
saan-saurabh <49774071+saan-saurabh@users.noreply.github.com>
2020-11-08 20:26:03 +0530

    new commit

--- projectFrontend/src/core/Base.js
+++ projectFrontend/src/core/Base.js
@@ -23,7 +23,7 @@ const Base = ({
       </div>
       <div className="container">
         <span className="text-muted">
-          An Amazing <span className="text-white">Versatile E-commerce Website</span>
+          An Amazing <span className="text-white">LPUCart E-commerce Website</span>
         </span>
       </div>
     </footer>
